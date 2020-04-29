e82e2b7cb32f88641e7e366caf1c4bb1e5301749
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-04-29 10:57:03 +0530

    s3 bucket without variables

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ provider "aws"{
   version = "2.33.0"
   region="ap-south-1"
 }
+
+resource "aws_s3_bucket" "s3_client_bucket" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
