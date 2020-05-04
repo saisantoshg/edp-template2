d7a28e059991fceefd381dc5aa3249b0bd217e3c
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-04 11:44:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,13 +2,13 @@ variable "s3_client_buckets"{
    type = string
 }
 variable "client_env" {
-    type= string
+    type = string
 }
 variable "iam_glue_policy_arn" {
   description = "IAM Policy to be attached to role"
   type = list(string)
 }
 variable "client_users_bucket"{
-   type=map
+   type = map
    description ="mapping client users and clients s3 buckets"
 }
