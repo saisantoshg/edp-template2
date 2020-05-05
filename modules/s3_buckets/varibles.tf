76543ab95a54ff8d41fb70aa781d1ca418cd12e8
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-05 10:54:59 +0530

    Update varibles.tf

--- modules/s3_buckets/varibles.tf
+++ modules/s3_buckets/varibles.tf
@@ -2,9 +2,6 @@ variable "s3_client_buckets"{
    description = "list of s3 buckets to be created"
    type = list(string)
 }
-variable "client_env" {
-    type = string
-}
 variable "create_buckets"{
      description = "Controls whether to create s3 buckets"
      type = bool
