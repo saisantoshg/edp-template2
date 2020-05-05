ff5a7cd1c3f21c640daac5c3c786a06b56c6899f
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-05 13:13:17 +0530

    Update variables.tf

--- modules/users/variables.tf
+++ modules/users/variables.tf
@@ -1,9 +1,41 @@
-variable "create_users"{
-    type = bool
-   default = true
-   description ="should we create users?"
+variable "create_users" {
+  description = "Controls whether an IAM user will be created"
+  type        = bool
+  default     = true
 }
-variable "client_users_bucket_mapping"{
-   type = map(string)
-   description ="mapping client users and clients s3 buckets"
+
+variable "policy_arns" {
+  description = "List of all managed policy ARNs used in the users object. This is needed to properly order policy attachments/detachments on resource cycles"
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "Map of tags to apply to the IAM users. Merged with tags set per-user in the user-schema"
+  type        = map(string)
+  default     = {}
+}
+
+variable "users" {
+  description = "Schema list of IAM users"
+  type = list(object({
+    name                 = string
+    force_destroy        = bool
+    path                 = string
+    permissions_boundary = string
+    tags                 = map(string)
+    policy_arns          = list(string)
+    inline_policies = list(object({
+      name           = string
+      template       = string
+      template_paths = list(string)
+      template_vars  = map(string)
+    }))
+    access_keys = list(object({
+      name    = string
+      status  = string
+      pgp_key = string
+    }))
+  }))
+  default = []
 }
