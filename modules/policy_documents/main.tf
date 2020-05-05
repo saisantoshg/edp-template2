ad5fc9cdb4151b1d60cfa4893245f7abed3f3fc9
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-05 13:09:12 +0530

    Update main.tf

--- modules/policy_documents/main.tf
+++ modules/policy_documents/main.tf
@@ -1,1 +1,17 @@
+locals {
+  policies_map = { for item in var.policies : item.name => item }
+}
 
+data "external" "this" {
+  for_each = var.create_policy_documents ? local.policies_map : {}
+
+  program = ["python", "${path.module}/policy_document_handler.py", "-"]
+  query   = { for arg, val in each.value : arg => jsonencode(val) }
+}
+
+data "template_file" "this" {
+  for_each = var.create_policy_documents ? local.policies_map : {}
+
+  template = data.external.this[each.key].result["policy"]
+  vars     = each.value.template_vars
+}
