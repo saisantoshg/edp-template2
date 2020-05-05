1fe3c727141202304c5440adf959681057ff2f12
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-05 10:56:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,6 @@
 variable "s3_client_buckets"{
    type = list(string)
 }
-variable "client_env" {
-    type = string
-}
 variable "iam_glue_policy_arn" {
   description = "IAM Policy to be attached to role"
   type = list(string)
