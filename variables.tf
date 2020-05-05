d6fc2a59d55c996d3350ad7ae1fb7e0059de5b8c
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-05 13:23:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,3 +21,28 @@ variable "client_users_bucket_mapping"{
    type = map(string)
    description ="mapping client users and clients s3 buckets"
 }
+
+
+variable "users" {
+  description = "Schema list of IAM users"
+  type = list(object({
+    name                 = string
+    force_destroy        = bool
+    path                 = string
+    permissions_boundary = string
+    tags                 = map(string)
+    policy_arns          = list(string)
+    inline_policies = list(object({
+      name           = string
+      template       = string
+      template_paths = list(string)
+      template_vars  = map(string)
+    }))
+    access_keys = list(object({
+      name    = string
+      status  = string
+      pgp_key = string
+    }))
+  }))
+  default = []
+}
