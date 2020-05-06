0f026bedfe064b1e4961b6d3a609b4f008d92e87
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-06 19:07:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,10 @@ provider "aws"{
   region="ap-south-1"
 }
 
-module "s3_buckets" {
-  source = "./modules/s3_buckets/"
+module "groups" {
+  source = "./modules/groups/"
 
-  create_buckets           = var.create_buckets
-  s3_client_buckets        = var.s3_client_buckets
-
-}
-  
-module "glue_s3_role" {
-  source = "./modules/roles/"
-
-  create_role              = var.create_role
-  iam_glue_policy_arn      = var.iam_glue_policy_arn
+  group_map      = var.group_map
 
 }
 
- 
