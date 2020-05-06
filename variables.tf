d171f91649442cf11b68b880ce7bd54c583987ac
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-06 19:06:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,3 @@
-variable "create_buckets"{
-   type = bool
-   default = true
-}
-variable "create_role"{
-   type = bool
-   default = true
-}
-variable "create_users"{
-   type = bool
-   default = true
-}
-variable "s3_client_buckets"{
-   type = list(string)
-}
-variable "iam_glue_policy_arn" {
-  description = "IAM Policy to be attached to role"
-  type = list(string)
-}
 variable "group_map" {
   description = "attaching policies to groups"
   type = map(string)
