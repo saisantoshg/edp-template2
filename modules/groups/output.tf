04583f520812fbf5763daa8330b232701723afb1
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-06 22:00:30 +0530

    Update output.tf

--- modules/groups/output.tf
+++ modules/groups/output.tf
@@ -1,4 +1,16 @@
-output "group_name"{
+output "group_name1"{
+    description = "flatten output values."
+    value  = aws_iam_group.SparxEA.name
+}
+output "group_name2"{
+    description = "flatten output values."
+    value  = aws_iam_group.EC2_RDS.name
+}
+output "group_name3"{
     description = "flatten output values."
     value  = aws_iam_group.ViewBilling.name
+}
+output "group_name3"{
+    description = "flatten output values."
+    value  = local.policy_attachments 
 }
