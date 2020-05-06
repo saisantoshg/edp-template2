c214af61d1be7091010c836a637955541b780836
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-06 19:20:42 +0530

    Delete varibles.tf

--- modules/s3_buckets/varibles.tf
+++
@@ -1,9 +0,0 @@
-variable "s3_client_buckets"{
-   description = "list of s3 buckets to be created"
-   type = list(string)
-}
-variable "create_buckets"{
-     description = "Controls whether to create s3 buckets"
-     type = bool
-     default = true
- }
