062228382aa29e802d6ff731306e2a928716a0da
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-06 20:25:11 +0530

    Update variables.tf

--- modules/groups/variables.tf
+++ modules/groups/variables.tf
@@ -2,6 +2,6 @@ variable "group_map" {
   description = "attaching policies to groups"
   type = list(object({
       groupname = string
-      policies = list(string)
+      policies = string
     }))
 }
