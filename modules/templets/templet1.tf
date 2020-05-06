7e0ff9c9316945c35b2ad3aefd01ae167caba1f7
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-06 19:21:00 +0530

    Delete templet1.tf

--- modules/templets/templet1.tf
+++
@@ -1,7 +0,0 @@
-data "templet_file" "user_policy_templet"{
-  templet = "${file("iam_user_s3_inlinepolicy.json")}"
-  vars = {
-      bucket_name = "client-1A"
-      bucket_name = "client-1A"
-  }
-}
