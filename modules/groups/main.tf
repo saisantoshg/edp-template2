2bc0103c2d90c06714133a166bbb1b8784116844
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-06 18:57:53 +0530

    Update main.tf

--- modules/groups/main.tf
+++ modules/groups/main.tf
@@ -3,6 +3,18 @@ provider "aws"{
   region="ap-south-1"
 }
 
+locals {
+  policy_attachments = flatten([
+    for groupname, policies in var.service_map : [
+      for policy_arn in policies: {
+        groupname   = groupname
+        policy_arn = policy_arn
+      }
+    ]
+  ])
+}
+
+
 resource "aws_iam_group" "EC2+RDS" {
   name = "EC2+RDS"
   path = "/groups/"
@@ -16,4 +28,14 @@ resource "aws_iam_group" "SparxEA" {
 resource "aws_iam_group" "ViewBilling" {
   name = "ViewBilling"
   path = "/groups/"
+}
+
+resource "aws_iam_group_policy_attachment" "test-attach" {
+  for_each = {
+    for up in var.policy_attachments :
+    "${up.groupname} ${up.policy_arn}" => up
+  }
+
+  user       = each.value.groupname
+  policy_arn = each.value.policy_arn
 }
