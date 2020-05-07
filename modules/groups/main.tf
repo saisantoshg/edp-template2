75cce47c74e006151f43da499118ce2326d53a6f
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-05-07 22:51:28 +0530

    Update main.tf

--- modules/groups/main.tf
+++ modules/groups/main.tf
@@ -1,6 +1,6 @@
 locals {
   policy_attachments = flatten([
-    for groupname, policies in var.group_map : [
+    for groupname,policies in var.group_map : [
       for policy_arn in policies: {
         groupname   = groupname
         policy_arn = policy_arn
@@ -25,11 +25,6 @@ resource "aws_iam_group" "ViewBilling" {
   path = "/groups/"
 }
     
-resource "aws_iam_group" "Clients" {
-  name = "Clients"
-  path = "/groups/"
-}
-
 resource "aws_iam_group_policy_attachment" "group_attach"{
     foreach = {
      for up in var.policy_attachments :
